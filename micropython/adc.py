169c561ee615495d1842993e8d43130aad845ae1
nmctseb <29257008+nmctseb@users.noreply.github.com>
2018-10-10 16:38:55 +0200

    fix adc example with attn

--- micropython/adc.py
+++ micropython/adc.py
@@ -1,15 +1,19 @@
+import sys
+
 import machine
 
 adc = machine.ADC()  # create an ADC object
 
+
 # ATTN_0DB ATTN_11DB ATTN_2_5DB ATTN_6DB
 # WIDTH_9BIT WIDTH_10BIT WIDTH_11BIT WIDTH_12BIT
 
+def adc_attn(pin: str):  # LoPy4
+    ch = adc.channel(pin=pin, attn=machine.ADC.ATTN11DB)
+    return ch.value()
 
-adc.atten(machine.ADC.ATTN_11DB)
 
-
-def adc_read(pin):
+def adc_read(pin: int):  # ESP32 (to be tested)
     pin = machine.Pin(pin)
     ch = adc.channel(pin=pin)  # create an analog pin on GP3
     val = ch.value()  # read an analog value
@@ -27,3 +31,8 @@ def adc_calibrate(pin='P22'):  # LoPy only?
         vref = int(val)
 
     adc.vref(vref)
+
+
+if __name__ == "__main__":
+    if sys.platform == "LoPy4":
+        adc_calibrate()
