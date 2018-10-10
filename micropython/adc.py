93d91e60fb4e5bcf6350ea7ccac817bc7dd6663c
nmctseb <29257008+nmctseb@users.noreply.github.com>
2018-10-10 17:04:03 +0200

    fix adc example for esp32

--- micropython/adc.py
+++ micropython/adc.py
@@ -2,37 +2,35 @@ import sys
 
 import machine
 
-adc = machine.ADC()  # create an ADC object
-
-
 # ATTN_0DB ATTN_11DB ATTN_2_5DB ATTN_6DB
 # WIDTH_9BIT WIDTH_10BIT WIDTH_11BIT WIDTH_12BIT
 
-def adc_attn(pin: str):  # LoPy4
-    ch = adc.channel(pin=pin, attn=machine.ADC.ATTN11DB)
-    return ch.value()
+if sys.platform == 'LoPy4':
+    adc = machine.ADC()  # create an ADC object
+
+    def adc_read(pin: str):  # LoPy4
+        ch = adc.channel(pin=pin, attn=machine.ADC.ATTN11DB)
+        return ch.value()
+
+    def adc_calibrate(pin='P22'):  # LoPy only?
+        # Output Vref to pin
+        adc.vref_to_pin(pin)
+
+        vref = 0
+        while not 900 < vref < 1300:
+            print("Please measure the voltage on pin ", pin)
+            val = input("Voltage (mV): ")
+            vref = int(val)
+
+        adc.vref(vref)
 
 
-def adc_read(pin: int):  # ESP32 (to be tested)
-    pin = machine.Pin(pin)
-    ch = adc.channel(pin=pin)  # create an analog pin on GP3
-    val = ch.value()  # read an analog value
-    return val
+if sys.platform == 'esp32':
 
+    def adc_read(pin: int):  # ESP32 (to be tested)
+        pin = machine.Pin(pin)
+        adc = machine.ADC(pin)  # create an ADC object
+        adc.attn(machine.ADC.ATTN11DB)
+        val = adc.read()  # read an analog value
+        return val
 
-def adc_calibrate(pin='P22'):  # LoPy only?
-    # Output Vref to pin
-    adc.vref_to_pin(pin)
-
-    vref = 0
-    while not 900 < vref < 1300:
-        print("Please measure the voltage on pin ", pin)
-        val = input("Voltage (mV): ")
-        vref = int(val)
-
-    adc.vref(vref)
-
-
-if __name__ == "__main__":
-    if sys.platform == "LoPy4":
-        adc_calibrate()
