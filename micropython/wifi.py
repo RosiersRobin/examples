1fb6946187b99841a1d43982af54a58a0d002afb
Robin Rosiers <robin.rosiers@student.howest.be>
2018-10-17 16:51:15 +0200

    Fixed the wifi connector according to the documentation. for LoPy

--- micropython/wifi.py
+++ micropython/wifi.py
@@ -7,14 +7,16 @@ WPA_PSK = "88VNncvQGy"
 
 
 def do_connect(essid, wpa_psk):
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect(essid, wpa_psk)
-        while not wlan.isconnected():
-            time.sleep(1/10)
-    print('network config:', wlan.ifconfig())
+    wlan = network.WLAN(mode=network.WLAN.STA)
+    nets = wlan.scan()
+    for net in nets:
+        if net.ssid == essid:
+            print('Network found!')
+            wlan.connect(net.ssid, auth=(net.sec, wpa_psk), timeout=5000)
+            while not wlan.isconnected():
+                machine.idle()  # save power while waiting
+            print('WLAN connection succeeded!')
+            break
 
 
 if __name__ == "__main__":
